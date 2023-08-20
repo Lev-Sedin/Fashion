bee6a14184c982ab618cf0fcd84b76c42661d483
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2023-08-20 23:12:16 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,26 @@
-$(function(){
-   
-})
-const btnOpen = document.querySelector('.modal__btn')
-const btnClose = document.querySelector('.modal__close')
-const modalWindow = document.querySelector('.modal')
-const btnCancel = document.querySelector('.modal__cancel')
-console.log(modalWindow)
 
-btnOpen.addEventListener('click', function(){
-    modalWindow.style.display = 'flex'
-})
-btnClose.addEventListener('click',function(){
-    modalWindow.style.display = ''
-})
-btnCancel.addEventListener('click',function(){
-    modalWindow.style.display = ''
-})
+
+const menu = () =>{
+    const btnOpen = document.querySelector('.btn-contact')
+    const btn = document.querySelector(".btn")
+    const nav = document.querySelector(".nav-menu__mobile")
+    const linkOne = document.querySelector(".link-one")
+    const linkTwo = document.querySelector(".link-two")
+    const btnClosed = document.querySelector(".btn-closed")
+
+
+    btnOpen.addEventListener('click', function(){
+        btn.classList.add("btn-contact__unactive")
+        nav.classList.add("nav-menu__mobile-active")
+        linkOne.classList.add("link-unactive__one")
+        linkTwo.classList.add("link-unactive__two")
+    })
+    btnClosed.addEventListener("click", function(){
+        nav.classList.remove("nav-menu__mobile-active")
+        linkOne.classList.remove("link-unactive__one")
+        linkTwo.classList.remove("link-unactive__two")
+        btn.classList.remove("btn-contact__unactive")
+    })
+}
+
+menu() //паралакс
